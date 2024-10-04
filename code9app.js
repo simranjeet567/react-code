a0989329ef10f901992e66e3212a08b82ba3c50b
SimranjeetSingh <simranjeet777000@gmail.com>
2024-10-04 12:58:03 +0530

    replacement of the content of first <div> tag by the parent content.

---
+++ code9app.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child1"}, [
+            React.createElement("h1", {id: "heading1"}, "I'm h1 tag"),
+            React.createElement("h2", {id:"heading2"}, "I'm h2 tag")
+        ]),
+    React.createElement("div", {id: "child2"}, [
+            React.createElement("h1", {id: "head1"}, "I'm h1 Tag"),
+            React.createElement("h2", {id:"head2"}, "I'm h2 Tag")
+        ])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("headerId"));
+
+root.render(parent);
