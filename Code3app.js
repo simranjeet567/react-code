8fd5640e60470af28cf99428096ddad5270610a1
SimranjeetSingh <simranjeet777000@gmail.com>
2024-10-07 18:39:45 +0530

    Hands on with React. Playing with package.json & package-lock.json files. Tinkering with gitignore file.

--- Code3app.js
+++ Code3app.js
@@ -1,4 +1,7 @@
-const heading = React.createElement("h1", {id: "heading1"}, "Hi, from React")
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+
+const heading = React.createElement("h1", {id: "heading1"}, "Hi, from React")
 const root = ReactDOM.createRoot(document.getElementById("rootId"));
 root.render(heading);
